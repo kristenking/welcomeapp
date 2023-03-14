f25327792a4cc5200109b9a7600caccba09c0440
kristenking <kristenking0110@gmail.com>
2023-03-13 22:52:30 -0400

    Initialized project

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,11 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = ['Fold laundry', 'Sweep porch', 'Wash dishes', 'Mow lawn']
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  
+  root "main#index"
+
+  match 'about', to: "main#about", via: :get
+  match 'hello', to: "main#hello", via: :get
+  
+  get 'main/index'
+  get 'tasks/index'
+  get 'tasks/new'
+  get 'tasks/edit'
+  get 'posts/index'
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
